37e02f37653af953d695490dc8a8d91ebd978b28
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-07-04 15:36:49 +0300

    init

---
+++ EraMatch/Models/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  EraMatch
+//
+//  Created by R. Metehan GÖKTAŞ on 4.07.2024.
+//
+
+import Foundation
+
---
+++ EraMatch/Services/SignInService.swift
@@ -0,0 +1,8 @@
+//
+//  SignInService.swift
+//  EraMatch
+//
+//  Created by R. Metehan GÖKTAŞ on 4.07.2024.
+//
+
+import Foundation
