332e4fe6089e0abe90d0b88757b69dde77678a04
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-07-20 19:41:13 +0300

    CREATED ImagePickerView

---
+++ EraMatch/Views/PublicStateViews/ImagePickerView.swift
@@ -0,0 +1,52 @@
+//
+//  ImagePickerView.swift
+//  EraMatch
+//
+//  Created by R. Metehan GÖKTAŞ on 20.07.2024.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType
+    var completionHandler: (UIImage?) -> Void
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self, completionHandler: completionHandler)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePickerView
+        var completionHandler: (UIImage?) -> Void
+
+        init(_ parent: ImagePickerView, completionHandler: @escaping (UIImage?) -> Void) {
+            self.parent = parent
+            self.completionHandler = completionHandler
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                completionHandler(image)
+            } else {
+                completionHandler(nil)
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            completionHandler(nil)
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
