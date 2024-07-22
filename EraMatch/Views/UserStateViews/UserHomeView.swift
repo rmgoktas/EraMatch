82d945bdcf40eab5eb6a2ee10f6298b599b72fee
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-07-22 20:14:57 +0300

    FIX

--- EraMatch/Views/UserStateViews/UserHomeView.swift
+++ EraMatch/Views/UserStateViews/UserHomeView.swift
@@ -8,119 +8,200 @@
 import SwiftUI
 
 struct UserHomeView: View {
-    @StateObject private var viewModel = UserHomeViewModel()
+    @StateObject private var homeViewModel = UserHomeViewModel()
     @State private var isMenuOpen = false
     @EnvironmentObject var loginViewModel: LoginViewModel
+    @State private var selectedTab: String = "Home" // Selected tab state
 
     var body: some View {
-        NavigationView {
-            ZStack {
-                VStack {
-                    // Üst bar
-                    HStack {
-                        Button(action: {
-                            withAnimation {
-                                isMenuOpen.toggle()
-                            }
-                        }) {
-                            Image(systemName: "line.horizontal.3")
-                                .font(.title)
-                                .foregroundColor(.white)
+        ZStack {
+            VStack {
+                HStack {
+                    Button(action: {
+                        withAnimation {
+                            isMenuOpen.toggle()
                         }
-                        
-                        Spacer()
-                        
-                        Text("Home")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                        
-                        Spacer()
-                        
-                        Image(systemName: "magnifyingglass")
+                    }) {
+                        Image(systemName: "line.horizontal.3")
                             .font(.title)
                             .foregroundColor(.white)
                     }
-                    .padding()
-                    .background(Color.blue)
                     
-                    // Ana içerik
-                    ScrollView {
+                    Spacer()
+                    
+                    Text("Home")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "magnifyingglass")
+                        .font(.title)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                // Ana içerik
+                ScrollView {
+                    VStack {
+                        Text("Hi, \(homeViewModel.userName)!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.top, 25)
+                            .padding(.trailing, 100)
+                        
                         VStack {
-                            Text("Hi, \(viewModel.userName)!")
-                                .font(.largeTitle)
+                            Text("What to Do ?")
+                                .font(.title2)
                                 .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding(.top, 25)
-                                .padding(.trailing, 100)
-                            
-                            VStack {
-                                Text("What to Do ?")
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                    .padding(.top, 20)
-                                
-                                HStack {
-                                    Image(systemName: "airplane")
-                                        .font(.title2)
-                                    VStack(alignment: .leading) {
-                                        Text("Search Free Travels")
-                                            .font(.headline)
-                                        Text("Find and filter travel events")
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                    }
-                                    Spacer()
-                                    Image(systemName: "chevron.right")
-                                }
-                                .padding()
-                                .background(Color.white)
-                                .cornerRadius(15)
-                                .shadow(radius: 5)
-                                
-                                // Event by Topics
-                                VStack(alignment: .leading) {
-                                    Text("Events by Topics")
-                                        .font(.title3)
-                                        .padding(.top, 20)
-                                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
-                                        ForEach(viewModel.topics, id: \.self) { topic in
-                                            Text(topic)
-                                                .padding()
-                                                .background(Color.white)
-                                                .cornerRadius(15)
-                                                .shadow(radius: 5)
-                                        }
-                                    }
-                                }
                                 .padding(.top, 20)
+                            
+                            HStack {
+                                Image(systemName: "airplane")
+                                    .font(.title2)
+                                VStack(alignment: .leading) {
+                                    Text("Search Free Travels")
+                                        .font(.headline)
+                                    Text("Find and filter travel events")
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                                Image(systemName: "chevron.right")
                             }
                             .padding()
                             .background(Color.white)
-                            .cornerRadius(30)
-                            .padding(.top, -20)
+                            .cornerRadius(15)
+                            .shadow(radius: 5)
+                            
+                            // Event by Topics
+                            VStack(alignment: .leading) {
+                                Text("Events by Topics")
+                                    .font(.title3)
+                                    .padding(.top, 10)
+                                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
+                                    ForEach(homeViewModel.topics, id: \.self) { topic in
+                                        Text(topic)
+                                            .padding()
+                                            .background(Color.white)
+                                            .cornerRadius(15)
+                                            .shadow(radius: 5)
+                                    }
+                                }
+                            }
+                            .padding(.top, 30)
                         }
                         .padding()
+                        .background(Color.white)
+                        .cornerRadius(25)
+                        .padding(.top, 50)
                     }
-                    .background(Color.blue.edgesIgnoringSafeArea(.all))
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    
                 }
+            }
+            .background(BackgroundView())
+            
+            VStack {
+                Spacer()
+                UserNavBarView(selectedTab: $selectedTab)
+                    .padding(.horizontal, 10)
+                    .frame(maxWidth: .infinity)
+                    
+            }
+            
+            if isMenuOpen {
+                Color.black.opacity(0.5)
+                    .edgesIgnoringSafeArea(.all)
+                    .transition(.opacity)
+                    .onTapGesture {
+                        withAnimation {
+                            isMenuOpen = false
+                        }
+                    }
                 
-                // Yan menü
-                if isMenuOpen {
-                    UserSliderView(isMenuOpen: $isMenuOpen)
-                }
+                sliderMenu
+                    .transition(.move(edge: .leading))
+                    .animation(.easeInOut(duration: 0.3))
             }
         }
         .onAppear {
-            viewModel.fetchUsername()
+            homeViewModel.fetchUsername()
         }
         .navigationBarBackButtonHidden(true)
+    }
+
+    // Slider menü bileşeni
+    var sliderMenu: some View {
+        ZStack(alignment: .leading) {
+            BackgroundView()
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("EraMatch")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            isMenuOpen = false
+                        }
+                    }) {
+                        Image(systemName: "arrow.backward")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding()
+                
+                List {
+                    NavigationLink(destination: GuideScreenView(content: .whatIsEraMatch)) {
+                        Text("What is EraMatch ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .howDoIParticipate)) {
+                        Text("How Do I Participate ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .doIHaveToPay)) {
+                        Text("Do I Have to Pay ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .whatCanIDo)) {
+                        Text("What Can I Do on EraMatch ?")
+                    }
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    loginViewModel.logoutUser()
+                    withAnimation {
+                        isMenuOpen = false
+                    }
+                }) {
+                    HStack {
+                        Text("Sign Out")
+                        Image(systemName: "arrow.right.circle")
+                    }
+                    .padding(.bottom, 50)
+                    .padding(.leading, 180)
+                }
+            }
+            .frame(width: 300)
+            .background(Color.white)
+            .offset(x: isMenuOpen ? 0 : -300, y: 50)
+            .edgesIgnoringSafeArea(.top)
+        }
     }
 }
 
 struct UserHomeView_Previews: PreviewProvider {
     static var previews: some View {
         UserHomeView()
+            .environmentObject(LoginViewModel())
     }
 }
+
+
 
 
