1182f575c6a93b6823107c26380d0cba674e2f92
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-07-26 18:18:28 +0300

    CREATED NgoHomeView

--- EraMatch/Views/NgoStateViews/NgoHomeView.swift
+++ EraMatch/Views/NgoStateViews/NgoHomeView.swift
@@ -9,11 +9,56 @@ import SwiftUI
 
 struct NgoHomeView: View {
     var body: some View {
-            Text("Ngo Home Screen")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.red)
-                .navigationBarBackButtonHidden(true)
+        ZStack {
+            BackgroundView()
+
+            VStack(spacing: 30) {
+                // My Events Header
+                Text("My Events")
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal, 90)
+                    .background(Color.white)
+                    .foregroundColor(.black)
+                    .cornerRadius(30)
+                    .shadow(radius: 4)
+
+                // Event Card
+                EventCardView(
+                    title: "Hidden Geniuses",
+                    subtitle: "Training Course on “Digital Skills”",
+                    location: "Madrid, SPAIN",
+                    dateRange: "14 Apr 2024 - 21 Apr 2024",
+                    onDetailTap: {
+                        // Navigate to event details
+                    }
+                )
+
+                Spacer()
+
+                // Floating Action Button
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // Add new event action
+                        }) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.purple)
+                                .cornerRadius(30)
+                        }
+                        .padding()
+                    }
+                }
+
+                // NGO Nav Bar
+                NgoNavBarView()
+                    .padding(.top, 10)
+            }
+        }
     }
 }
 
@@ -22,3 +67,6 @@ struct NgoHomeView_Previews: PreviewProvider {
         NgoHomeView()
     }
 }
+
+
+
