5e7b95d3c7ca8529ffdd364854023e0a3bfd0615
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-07-28 18:55:34 +0300

    ui fix, added mockup data

--- EraMatch/Views/NgoStateViews/NgoHomeView.swift
+++ EraMatch/Views/NgoStateViews/NgoHomeView.swift
@@ -1,5 +1,5 @@
 //
-//  AdminHomeView.swift
+//  NgoHomeView.swift
 //  EraMatch
 //
 //  Created by R. Metehan GÖKTAŞ on 4.07.2024.
@@ -8,33 +8,107 @@
 import SwiftUI
 
 struct NgoHomeView: View {
+    @State private var isMenuOpen = false
+    @EnvironmentObject var loginViewModel: LoginViewModel
+
     var body: some View {
-        ZStack {
-            BackgroundView()
+        NavigationView {
+            ZStack {
+                BackgroundView()
 
-            VStack(spacing: 30) {
-                // My Events Header
-                Text("My Events")
-                    .font(.title)
-                    .bold()
-                    .padding(.horizontal, 90)
-                    .background(Color.white)
-                    .foregroundColor(.black)
-                    .cornerRadius(30)
-                    .shadow(radius: 4)
+                VStack {
+                    // Üst çubuk
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                isMenuOpen.toggle()
+                            }
+                        }) {
+                            Image(systemName: "line.horizontal.3")
+                                .font(.title)
+                                .foregroundColor(.white)
+                        }
 
-                // Event Card
-                EventCardView(
-                    title: "Hidden Geniuses",
-                    subtitle: "Training Course on “Digital Skills”",
-                    location: "Madrid, SPAIN",
-                    dateRange: "14 Apr 2024 - 21 Apr 2024",
-                    onDetailTap: {
-                        // Navigate to event details
+                        Spacer()
+
+                        Text("My Events")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.white)
+
+                        Spacer()
+
+                        Image(systemName: "magnifyingglass")
+                            .font(.title)
+                            .foregroundColor(.white)
                     }
-                )
+                    .padding(.top, 30)
+                    .padding(.horizontal)
+                    .background(Color.black.opacity(0))
 
-                Spacer()
+                    GeometryReader { geometry in
+                        ScrollView {
+                            VStack(spacing: 30) {
+                                // Event Cards
+                                EventCardView(
+                                    title: "Hidden Geniuses",
+                                    subtitle: "Training Course on “Digital Skills”",
+                                    location: "Madrid, SPAIN",
+                                    dateRange: "14 Apr 2024 - 21 Apr 2024",
+                                    onDetailTap: {
+                                        // Navigate to event details
+                                    }
+                                )
+
+                                EventCardView(
+                                    title: "Youth Exchange",
+                                    subtitle: "Exploring Cultural Diversity",
+                                    location: "Berlin, GERMANY",
+                                    dateRange: "10 Jun 2024 - 20 Jun 2024",
+                                    onDetailTap: {
+                                        // Navigate to event details
+                                    }
+                                )
+
+                                EventCardView(
+                                    title: "Environmental Summit",
+                                    subtitle: "Actions for a Sustainable Future",
+                                    location: "Oslo, NORWAY",
+                                    dateRange: "5 Jul 2024 - 12 Jul 2024",
+                                    onDetailTap: {
+                                        // Navigate to event details
+                                    }
+                                )
+
+                                Spacer(minLength: 90) // Floating Action Button için altta yer bırakmak amacıyla spacer ekledim
+                            }
+                            .padding(.top, geometry.safeAreaInsets.top + 60)
+                        }
+                    }
+                }
+
+                // NGO Nav Bar
+                VStack {
+                    Spacer()
+                    NgoNavBarView()
+                        .padding(.horizontal)
+                        .padding(.top, 10)
+                }
+
+                if isMenuOpen {
+                    Color.black.opacity(0.5)
+                        .edgesIgnoringSafeArea(.all)
+                        .transition(.opacity)
+                        .onTapGesture {
+                            withAnimation {
+                                isMenuOpen = false
+                            }
+                        }
+
+                    sliderMenu
+                        .transition(.move(edge: .leading))
+                        .animation(.easeInOut(duration: 0.3))
+                }
 
                 // Floating Action Button
                 VStack {
@@ -45,19 +119,80 @@ struct NgoHomeView: View {
                             // Add new event action
                         }) {
                             Image(systemName: "plus")
+                                .font(.system(size: 32))
                                 .foregroundColor(.white)
-                                .padding()
+                                .padding(20)
                                 .background(Color.purple)
-                                .cornerRadius(30)
+                                .cornerRadius(40)
                         }
-                        .padding()
+                        .padding(.trailing, 20)
+                        .padding(.bottom, 90)
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+
+    // Slider menü bileşeni
+    var sliderMenu: some View {
+        ZStack(alignment: .leading) {
+            BackgroundView()
+
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("EraMatch")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            isMenuOpen = false
+                        }
+                    }) {
+                        Image(systemName: "arrow.backward")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding()
+
+                List {
+                    NavigationLink(destination: GuideScreenView(content: .whatIsEraMatch)) {
+                        Text("What is EraMatch ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .howDoIParticipate)) {
+                        Text("How Do I Participate ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .doIHaveToPay)) {
+                        Text("Do I Have to Pay ?")
+                    }
+                    NavigationLink(destination: GuideScreenView(content: .whatCanIDo)) {
+                        Text("What Can I Do on EraMatch ?")
                     }
                 }
 
-                // NGO Nav Bar
-                NgoNavBarView()
-                    .padding(.top, 10)
+                Spacer()
+
+                Button(action: {
+                    loginViewModel.logoutUser()
+                    withAnimation {
+                        isMenuOpen = false
+                    }
+                }) {
+                    HStack {
+                        Text("Sign Out")
+                        Image(systemName: "arrow.right.circle")
+                    }
+                    .padding(.bottom, 50)
+                    .padding(.leading, 180)
+                }
             }
+            .frame(width: 300)
+            .background(Color.white)
+            .offset(x: isMenuOpen ? 0 : -300, y: 50)
+            .edgesIgnoringSafeArea(.top)
         }
     }
 }
@@ -65,8 +200,15 @@ struct NgoHomeView: View {
 struct NgoHomeView_Previews: PreviewProvider {
     static var previews: some View {
         NgoHomeView()
+            .environmentObject(LoginViewModel())
     }
 }
+
+
+
+
+
+
 
 
 
