06feab6e1afa4ea641c07d2e3676ca2436c32342
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-12-23 02:41:23 +0300

    ui update

--- EraMatch/Views/PublicStateViews/NotificationsView.swift
+++ EraMatch/Views/PublicStateViews/NotificationsView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct NotificationView: View {
-    @Binding var shouldDismiss: Bool // Dismiss için binding
+    @Binding var shouldDismiss: Bool
 
     var body: some View {
         NavigationView {
             VStack {
+                Image(systemName: "bell.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .opacity(0.5)
+                Text("You have no notifications yet. Check back later!")
+                    .font(.headline)
+                    .foregroundColor(.gray) 
             }
             .navigationBarTitle("Notifications", displayMode: .inline)
             .navigationBarItems(trailing: Button("Close") {
-                shouldDismiss = false 
+                shouldDismiss = false
             })
         }
     }
